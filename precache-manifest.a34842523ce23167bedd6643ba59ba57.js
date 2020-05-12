96083b666cc7615a3a6e70976fd1041d957c947e
JKosk <you@example.com>
2020-05-12 14:13:10 +0200

    Updates

---
+++ precache-manifest.a34842523ce23167bedd6643ba59ba57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcb61a323242dd43e0f2509d508eef5",
+    "url": "/.github.io/Universe-Travel/index.html"
+  },
+  {
+    "revision": "d67c5de3cddf339192ac",
+    "url": "/.github.io/Universe-Travel/static/css/2.e5c964ac.chunk.css"
+  },
+  {
+    "revision": "84b9850440ee436581e5",
+    "url": "/.github.io/Universe-Travel/static/css/main.de75cc14.chunk.css"
+  },
+  {
+    "revision": "d67c5de3cddf339192ac",
+    "url": "/.github.io/Universe-Travel/static/js/2.8f8034d4.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/.github.io/Universe-Travel/static/js/2.8f8034d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b9850440ee436581e5",
+    "url": "/.github.io/Universe-Travel/static/js/main.7e060dd3.chunk.js"
+  },
+  {
+    "revision": "8367a4bb2ac1c64d65a9",
+    "url": "/.github.io/Universe-Travel/static/js/runtime-main.a6508b51.js"
+  }
+]);
